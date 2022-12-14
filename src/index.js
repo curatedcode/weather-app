8f301e1a86ec6e1ff2b08c8645c6f9b7a0dae58a
zack <zackaryfotheringham@hotmail.com>
2022-12-14 08:46:40 -0500

    trigger functions to check validity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import {checkState, verifyData } from './display'
 import './styles.css'
+
+document.querySelector('.get-weather').addEventListener('click',verifyData)
+
+document.querySelector('select[name="country"]').onchange = checkState
+document.querySelector('input[name="city"]').onchange = checkState
